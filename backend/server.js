e8740f47b6d7aa504d55a6de8b3c3a75f0750569
Alter80 <fahim.atul0@gmail.com>
2023-02-17 05:33:26 +0600

    done setup backend server

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const products = require("./data/products");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+app.get("/api/products", (req, res) => {
+  res.json(products);
+});
+
+app.get("/api/products/:id", (req, res) => {
+  const product = products.find(
+    (singleProduct) => singleProduct._id === req.params.id
+  );
+  res.json(product);
+});
+
+const port = 5000;
+app.listen(port, console.log(`Server Running on port ${port}`));
