f6bd32e79f7de607eb0505117427e7ab787784f4
Alter80 <fahim.atul0@gmail.com>
2023-10-16 14:14:13 +0600

    fix bug of action and constants redux

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,34 +1,38 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Col, Row } from "react-bootstrap";
 import Products from "../components/Products";
 import { Link } from "react-router-dom";
-import axios from "axios";
+import { listProducts } from "../actions/productActions";
 
 const HomeScreen = () => {
-  const [products, setProducts] = useState([]);
+  const dispatch = useDispatch();
+  const productList = useSelector((state) => state.productList);
+
+  const { loading, error, products } = productList;
 
   useEffect(() => {
-    const fetchProducts = async () => {
-      const { data } = await axios.get("/api/products");
-
-      setProducts(data);
-    };
-
-    fetchProducts();
-  }, []);
+    dispatch(listProducts());
+  }, [dispatch]);
 
   return (
     <>
       <h1>Latest Products</h1>
-      <Row>
-        {products.map((product) => (
-          <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-            <Link to={`/product/${product._id}`}>
-              <Products product={product} />
-            </Link>
-          </Col>
-        ))}
-      </Row>
+      {loading ? (
+        <h1>Loading....</h1>
+      ) : error ? (
+        <h3>{error}</h3>
+      ) : (
+        <Row>
+          {products.map((product) => (
+            <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+              <Link to={`/product/${product._id}`}>
+                <Products product={product} />
+              </Link>
+            </Col>
+          ))}
+        </Row>
+      )}
     </>
   );
 };
